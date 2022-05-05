4ecd3c047fd5e01a1fc7d3f5c997218ad61e40e1
erdemiremre <73900322+erdemiremre@users.noreply.github.com>
2022-05-06 01:26:18 +0300

    update

--- introduction.js
+++ introduction.js
@@ -1,1 +1,21 @@
-console.log('bu yazi js dosyasından geldi....')
+//console.log('bu yazi js dosyasından geldi....')
+
+//typeof ile değişkenlerin tipini buluruz..
+console.log(typeof'emre')
+console.log(typeof 5)
+console.log(typeof undefined)
+console.log(typeof true)
+console.log(typeof null)
+
+let firstName = 'Emre'
+let lastName = 'Erdemir'
+let country = 'Turkiye'
+let city = 'Istanbul'
+let age = 27
+let isMarried = false
+
+//sabit olanları const ile tanimlariz.sonradan deger atamasi yapamayiz
+const PI=3.14
+const gravity=9.81
+
+console.log(firstName , lastName , country ,city ,age , isMarried)
