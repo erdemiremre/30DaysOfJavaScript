7ff69a6261a7b6acfe2ce7302b43ccc6c76481af
erdemiremre <73900322+erdemiremre@users.noreply.github.com>
2022-05-27 22:04:56 +0300

    guncellendi.

--- koordinat.js
+++ koordinat.js
@@ -86,3 +86,16 @@ AddPoint = () => {
 
     _Draw.setActive(true);
 }
+
+
+const modalBtn = document.getElementById("modal-ac");
+const modal = document.querySelector(".modal");
+const modalKapat = document.getElementById("modal-kapat");
+
+modalBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+modalKapat.addEventListener("click", () => {
+  modal.style.display = "none";
+});
