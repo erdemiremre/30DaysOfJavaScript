da5ef8caffbc303e161af8465d9ab4c29106cd67
erdemiremre <73900322+erdemiremre@users.noreply.github.com>
2022-05-28 11:24:56 +0300

    update

--- koordinat.js
+++ koordinat.js
@@ -85,19 +85,11 @@ AddInteraction = () => {
 AddPoint = () => {
 
     _Draw.setActive(true);
+    let btn = document.getElementById('modal-ac')
+    btn.click();
+    
 }
 
-const modalBtn = document.getElementById("modal-ac");
-const modal = document.querySelector(".modal");
-const modalKapat = document.getElementById("modal-kapat");
-
-modalBtn.addEventListener("click", () => {
-  modal.style.display = "flex";
-});
-
-modalKapat.addEventListener("click", () => {
-  modal.style.display = "none";
-});
 
 
 
